6fbc2da4761c89680fef909fe7cdf7942b0bb15d
ds112 <16520479@gm.uit.edu.vn>
2025-10-13 10:42:25 +0700

    refactor: reorganize and standardize internationalization files for improved clarity and maintainability; update authentication and common message structures

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -1,23 +1,23 @@
-import common from "./en/common.json";
-import profile from "./en/profile.json";
-import auth from "./en/auth.json";
-import toast from "./en/toast.json";
-import write from "./en/write.json";
-import demo from "./en/demo.json";
-import user from "./en/user.json";
-import home from "./en/home.json";
 import article from "./en/article.json";
+import auth from "./en/auth.json";
+import common from "./en/common.json";
+import demo from "./en/demo.json";
+import home from "./en/home.json";
+import profile from "./en/profile.json";
+import toast from "./en/toast.json";
+import user from "./en/user.json";
+import write from "./en/write.json";
 
 const enMessages = {
   common,
-  profile,
   auth,
   toast,
-  write,
-  demo,
+  profile,
   user,
   home,
   article,
+  write,
+  demo,
 };
 
 export default enMessages;
--- src/i18n/locales/vi.ts
+++ src/i18n/locales/vi.ts
@@ -1,23 +1,23 @@
-import common from "./vi/common.json";
-import profile from "./vi/profile.json";
-import auth from "./vi/auth.json";
-import toast from "./vi/toast.json";
-import write from "./vi/write.json";
-import demo from "./vi/demo.json";
-import user from "./vi/user.json";
-import home from "./vi/home.json";
 import article from "./vi/article.json";
+import auth from "./vi/auth.json";
+import common from "./vi/common.json";
+import demo from "./vi/demo.json";
+import home from "./vi/home.json";
+import profile from "./vi/profile.json";
+import toast from "./vi/toast.json";
+import user from "./vi/user.json";
+import write from "./vi/write.json";
 
 const viMessages = {
   common,
-  profile,
   auth,
   toast,
-  write,
-  demo,
+  profile,
   user,
   home,
   article,
+  write,
+  demo,
 };
 
 export default viMessages;
