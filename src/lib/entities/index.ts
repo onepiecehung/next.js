983727c551919208ee1ffd848d2dcdd1ea5a8174
ds112 <16520479@gm.uit.edu.vn>
2025-10-13 14:39:19 +0700

    feat: add ReactionEntity to manage user reactions with support for various content types; update index to export the new entity

--- src/lib/entities/index.ts
+++ src/lib/entities/index.ts
@@ -3,5 +3,6 @@ export * from "./bookmark-folder.entity";
 export * from "./bookmark.entity";
 export * from "./comment.entity";
 export * from "./media.entity";
+export * from "./reaction.entity";
 export * from "./user.entity";
 
