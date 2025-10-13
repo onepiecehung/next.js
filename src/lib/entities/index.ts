7d4fb58ce73a5ed2d8a7409c10cabfad67b9655f
ds112 <16520479@gm.uit.edu.vn>
2025-10-13 14:37:20 +0700

    feat: introduce CommentEntity to manage user comments with support for nested replies, media attachments, and moderation flags; update index to export the new entity

--- src/lib/entities/index.ts
+++ src/lib/entities/index.ts
@@ -1,6 +1,7 @@
 export * from "./article.entity";
 export * from "./bookmark-folder.entity";
 export * from "./bookmark.entity";
+export * from "./comment.entity";
 export * from "./media.entity";
 export * from "./user.entity";
 
